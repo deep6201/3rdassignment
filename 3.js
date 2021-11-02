391b588d164ed0ef7805f6d3e957fe9cfe79b771
deep6201 <90336103+deep6201@users.noreply.github.com>
2021-11-02 14:57:14 +0530

    Update 3.js

--- 3.js
+++ 3.js
@@ -1,3 +1,15 @@
-console.log(notdefined);    
-let undefined;
-console.log(undefined);
+
+// undefined js code
+
+console.log(" creating variable x but without giving a value to x");
+
+var x ;
+console.log(x); // here x is undefined as it is not assigned by any value.
+
+//not defined js code
+
+console.log("we are printing b without declaring it");
+var a=10;
+var c="ramesh";
+console.log(b); // here b is not declared and we are printing it so give not defined error.
+
